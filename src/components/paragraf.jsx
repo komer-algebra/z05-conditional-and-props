bcb962535b21c07937cb456d224cc8f5ebd272e4
Komer @ Algebra <komersmile@gmail.com>
2023-09-08 19:06:01 +0200

    rješeno, mislim

---
+++ src/components/paragraf.jsx
@@ -0,0 +1,10 @@
+export function ConditionalRendering(props){
+  return !isNaN(props.number) && ( props.number > 0 ?
+    <>
+      <p>Želim vam ugodan dan</p>
+    </> :
+    <>
+      <p>Želim vam ugodnu noć</p>
+    </>
+  );
+}
