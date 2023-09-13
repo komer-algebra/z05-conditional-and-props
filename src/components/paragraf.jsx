73a68e3623fc0ddec0259d417b0692934c8706bd
Komer @ Algebra <komersmile@gmail.com>
2023-09-13 22:53:26 +0200

    Asignment added to README file

--- src/components/paragraf.jsx
+++ src/components/paragraf.jsx
@@ -1,18 +1,21 @@
-import React from 'react';    // Imported whole library React from package react (*)
-                              // just to use React.Component (in this module)
+import React from 'react';  // Imported whole library React from pckg react (*)
+                            // just to use React.Component (in this module)
 
-class ConditionalRendering extends React.Component {  // using React.Component from React that we imported (*)
-  // constructor(props){
+class ConditionalRendering extends React.Component {
+                        // using React.Component from React that we imported (*)
+  // constructor(props){                              // constructor not needed
   //   super(props);
   // }
   render(){
-    return  typeof this.props.number === 'number'  && ( this.props.number > 0 ?
-      <>
-        <p>Želim vam ugodan dan</p>
-      </> :
-      <>
-        <p>Želim vam ugodnu noć</p>
-      </>
+    return (
+    <>
+      {
+        typeof this.props.number === 'number'  &&
+          this.props.number > 0 ?
+            <p>Želim vam ugodan dan</p> :
+            <p>Želim vam ugodnu noć</p>
+      }
+    </>
     );
   }
 }
